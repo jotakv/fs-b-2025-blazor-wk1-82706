c3dfb297b51c7271b2e18fa7edd2c504eddc05c0
KevinValdivieso <KevinValdivieso@003-0164>
2025-09-18 12:59:22 +0200

    Add project files.

---
+++ fs-b-2025-blazor-wk1-82706/Program.cs
@@ -0,0 +1,28 @@
+using fs_b_2025_blazor_wk1_82706.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
